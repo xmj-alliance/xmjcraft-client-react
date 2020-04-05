eeb871bf0f00af016ed722782517fe25e5c2275e
valorad <valorad@outlook.com>
2020-04-05 16:12:15 -0400

    init project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Initial Page of xmjcraft-client-react
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
