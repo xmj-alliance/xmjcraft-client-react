2649f5abb3ba87749e00812f0c96805f87de5dd4
valorad <valorad@outlook.com>
2020-04-19 15:02:36 -0400

    user delete

--- src/views/user/detail.tsx
+++ src/views/user/detail.tsx
@@ -3,62 +3,133 @@ import React, { useState } from "react";
 import UserDetail from "../../components/user/detail";
 import UserEdit from "../../components/user/edit";
 import "./detail.scss";
+import { useRoute, useLocation } from "wouter";
+import { loader } from "graphql.macro";
+import { useMutation, useQuery } from "@apollo/client";
+import CUDMessage from "../../models/cudMessage";
+import User from "../../models/user";
 
-
-// const getSingleDefs = loader("../../services/userGraph/getSingle.graphql");
+const deleteSingleDefs = loader("../../services/userGraph/deleteSingle.graphql");
+const getSingleDefs = loader("../../services/userGraph/getSingle.graphql");
+const getListDefs = loader("../../services/userGraph/getList.graphql");
 
 const UserDetailView = () => {
 
   const [isEditMode, setIsEditMode] = useState(false);
+  const params = useRoute("/users/:dbname")[1];
+  const [_, setLocation] = useLocation();
 
-  // const params = useRoute("/users/:dbname")[1];
+  let dbname = params?.dbname;
+
+  const [deleteUser, { loading: isDeleteExecuting, error: deleteError }] = useMutation(
+    deleteSingleDefs,
+    {
+      update: (cache, response) => {
+        const message = response.data.deleteUser as CUDMessage;
+        
+        if (!message.ok) {
+          return;
+        }
+        
+        if (!dbname) {
+          return;
+        }
+
+        // update getSingle cache (we have a problem here. not in fact evicted)
+
+        cache.evict(dbname)
+        cache.gc();
+
+        // cache.writeQuery({
+        //   query: getSingleDefs,
+        //   variables: {
+        //     dbname: dbname
+        //   },
+        //   data: {user: null},
+        // });
+
+        // update getList cache
+
+        const cacheResponse = cache.readQuery<{users: User[]}>({ query: getListDefs });
+
+        if (!cacheResponse) {
+          return;
+        }
+
+        const newData = {
+          users: cacheResponse.users.filter((u) => u.dbname !== dbname)
+        }
+
+        cache.writeQuery({
+          query: getListDefs,
+          data: newData,
+        });
+
+      }
+    }
+  );
 
   const onEditClick = () => {
     setIsEditMode(!isEditMode);
   };
 
-  const onDeleteClick = () => {
+  const onDeleteClick = async () => {
+
+    await deleteUser({
+      variables: {
+        dbname: dbname
+      }
+    });
+
+    if (deleteError) {
+      console.error(deleteError);
+      
+    } else {
+      console.log(`Delete success`);
+      setLocation("/users");
+    }
 
   };
 
-  // get user data
-    // I will skip checking whether dbname is undefined.
-    // If I write a condition here, the React hook will stop working. 
-    // You can google "Rendered fewer hooks than expected" error.
-  // let dbname = params?.dbname;
+  const { loading: isQueryLoading, error: queryError, data } = useQuery(
+    getSingleDefs,
+    {
+      variables: {
+        dbname: dbname
+      }
+    }
+  );
 
-  // const { loading: isQueryLoading, error, data } = useQuery(
-  //   getSingleDefs,
-  //   {
-  //     variables: {
-  //       dbname: dbname
-  //     }
-  //   }
-  // );
-
-  // if (error) {
-  //   console.error(error);
-  // }
-
-  // const user = data?.user as User;
+  if (queryError) {
+    console.error(queryError);
+  }
 
   return (
     <section className="UserDetail">
 
       <header>
 
-        <div className="viewControl">
-          <button onClick={onEditClick}>Edit</button>
-          <button className="danger" onClick={onDeleteClick}>Delete</button>
-        </div>
+        {
+          data && data.user && data.user.dbname?
+
+          <div className="viewControl">
+            <button onClick={onEditClick}>Edit</button>
+            <button className="danger" onClick={onDeleteClick}>Delete</button>
+          </div>
+
+          : null
+        }
 
       </header>
 
       <main>
 
-        <UserDetail />
-
-        
+        {
+          data && data.user?
+          <UserDetail />
+          : null
+        }
+       
         {
           isEditMode?
           <div className="editImaginedModalDialog">
@@ -66,13 +137,19 @@ const UserDetailView = () => {
             <UserEdit />
           </div>
           : null
-        
         }
 
 
       </main>
 
-
+      <footer>
+        <div className="info">
+          {isQueryLoading?<h2>Loading...</h2>:null}
+          {queryError?<h2>Error loading data!</h2>:null}
+          {isDeleteExecuting?<h2>Deleting...</h2>:null}
+          {deleteError?<h2>Failed to delete.</h2>:null}
+        </div>
+      </footer>
       
     </section>
   );
